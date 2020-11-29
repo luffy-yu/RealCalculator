67cd1c910826a95fda92822aa27718724efc4fa4
luffy-yu <yuliuchuan@gmail.com>
2020-11-29 16:01:49 +0800

    Initial Commit

---
+++ Shared/RealCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealCalculatorApp.swift
+//  Shared
+//
+//  Created by 于留传 on 2020/11/29.
+//
+
+import SwiftUI
+
+@main
+struct RealCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
