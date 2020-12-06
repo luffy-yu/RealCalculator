7534925e7e313ac9ac19ed87c7bbc246b9415961
luffy-yu <yuliuchuan@gmail.com>
2020-12-06 23:39:55 +0800

    update keyboard

--- Shared/NormalUIView.swift
+++ Shared/NormalUIView.swift
@@ -40,7 +40,7 @@ func get_color_by_str(_ name: String) -> Color
 }
 
 let hstack_space:CGFloat = 5
-let vstack_space:CGFloat = 10
+let vstack_space:CGFloat = 5
 
 struct CustomButton : View {
     let title: String
@@ -78,7 +78,7 @@ struct HStackButtons: View {
                              width: width,
                              height: height, bg:color)
             }
-        }.lineSpacing(hstack_space)
+        }.lineSpacing(vstack_space)
     }
 }
 
@@ -92,6 +92,11 @@ struct NormalUIView: View {
     let btn_width = UIScreen.main.bounds.size.width / 4.0
     let btn_height = UIScreen.main.bounds.size.height * 0.618 / 5.0
     
+    let primary_display_height = UIScreen.main.bounds.size.height * (1-0.618) * 0.618
+    
+    let history_display_height =
+        UIScreen.main.bounds.size.height * (1-0.7) * (1-0.618)
+    
     var btns0:[String] = ["N/s", "0", ".", "="]
     
     var btns1:[String] = ["1", "2", "3", "+"]
@@ -102,18 +107,26 @@ struct NormalUIView: View {
     
     var btns4:[String] = ["C", "±", "%", "÷"]
     
+    var rows: [GridItem] =
+            Array(repeating: .init(), count: 7)
+    
     var body: some View {
-        VStack(
-            alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/,
-            spacing: vstack_space
-        ){
-            Spacer()
+        LazyHGrid(rows: rows, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: hstack_space, pinnedViews: /*@START_MENU_TOKEN@*/[]/*@END_MENU_TOKEN@*/, content: {
+            Text("1 + 2")
+                .frame(width: available_width, height: nil, alignment: .trailing)
+                .padding(.trailing, 10)
+                .foregroundColor(Color.gray)
+                .font(.system(size: 60))
+            Text("0").frame(width: available_width, height: nil, alignment: .trailing)
+                .padding(.trailing, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color.white)
+                .font(.system(size: 100))
             HStackButtons(buttons: btns4, height: btn_height, width: btn_width)
             HStackButtons(buttons: btns3, height: btn_height, width: btn_width)
             HStackButtons(buttons: btns2, height: btn_height, width: btn_width)
             HStackButtons(buttons: btns1, height: btn_height, width: btn_width)
             HStackButtons(buttons: btns0, height: btn_height, width: btn_width)
-        }
+        })
         .frame(width: available_width, height: available_height, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         .background(Color.black)
 //        .scaledToFill()
